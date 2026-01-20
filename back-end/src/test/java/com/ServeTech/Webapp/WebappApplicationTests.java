db85bad3f498cbe28766f47a77bcb4e5141843ef
cruise09 <09bhagatsingh@gmail.com>
2026-01-20 23:45:28 +0530

    Initial Backend Code.

---
+++ back-end/src/test/java/com/ServeTech/Webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServeTech.Webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
